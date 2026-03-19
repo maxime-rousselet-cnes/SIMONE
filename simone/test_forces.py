9ddf92ec868ca6b012774d358eee02f88da2c0c3
maxime-rousselet-cnes <maxime.rousselet@cnes.fr>
2026-03-19 16:23:37 +0100

    Minor change

--- simone/test_forces.py
+++ simone/test_forces.py
@@ -124,6 +124,7 @@ class TimeTestForceParameters(Parameters):
         time-dependent parameter.
         """
 
+        # TODO: Has the string to be consistent with variable name?
         return {"time_dependent_parameter": self.time_dependent_parameter.to_serializable()}
 
 
